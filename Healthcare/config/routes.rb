a8047bfed16a37ba2a73171b2e493cdb3c4e71ff
Rhys Gibson <rhysgibson@me.com>
2018-10-23 12:57:25 +1030

    Created Pages for features, will work on home page now

--- Healthcare/config/routes.rb
+++ Healthcare/config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'site/findClinics'
+  get 'site/highVisibility'
+  get 'site/lovedOnes'
+  get 'site/notifications'
   get 'site/home'
+
+  root 'site#home'
 
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
