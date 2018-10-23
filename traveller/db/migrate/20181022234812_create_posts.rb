61e5c84e1449430854c8a299695e52b271201aad
Rhys Gibson <rhysgibson@me.com>
2018-10-23 10:32:04 +1030

    troubleshooting database

--- traveller/db/migrate/20181022234812_create_posts.rb
+++ traveller/db/migrate/20181022234812_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.string :title
+      t.String :title
       t.text :body
 
       t.timestamps
