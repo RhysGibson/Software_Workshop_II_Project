3739621ed33df902512c984248c35c06ce89fc90
Rhys Gibson <rhysgibson@me.com>
2018-11-01 16:11:43 +1030

    Beginning of user accounts and log in

--- Healthcare/config/routes.rb
+++ Healthcare/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/new'
   resources :clinics
   get 'site/home'
   get 'site/findClinics'
@@ -11,11 +12,13 @@ Rails.application.routes.draw do
   get 'site/prescriptions'
 
   #if adding to databases comment out this root and uncomment which one you are working with
-  root 'site#home'
+  # root 'site#home'
 
   #Easy way to add to Clinics database
   # root 'clinics#index'
 
+  #User DB
+  root 'users#new'
 
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- Healthcare/db/schema.rb
+++ Healthcare/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_01_040404) do
+ActiveRecord::Schema.define(version: 2018_11_01_053235) do
 
   create_table "clinics", force: :cascade do |t|
     t.float "latitude"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 2018_11_01_040404) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
